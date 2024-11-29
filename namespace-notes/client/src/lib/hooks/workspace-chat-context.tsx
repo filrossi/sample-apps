2388073781d72c04109404ea8549a9cd7066c205
filrossi <filippo.rossi.fr@gmail.com>
2024-11-29 16:26:34 +0100

    testing - delete

--- namespace-notes/client/src/lib/hooks/workspace-chat-context.tsx
+++ namespace-notes/client/src/lib/hooks/workspace-chat-context.tsx
@@ -26,7 +26,8 @@ export const useWorkspaceChatContext = () => useContext(WorkspaceChatContext);
 
 const defaultWorkspaces: Workspace[] = [
   { id: 'default', name: 'Richard Feynman Lectures', locked: true, createdAt: 1, fileUrls: [] },
-  { id: 'empty', name: 'Empty Workspace', locked: true, createdAt: 2, fileUrls: [] },  
+  { id: 'empty', name: 'Empty Workspace', locked: true, createdAt: 2, fileUrls: [] },
+  { id: 'dutch-building-code', name: 'Dutch Building Code', locked: true, createdAt: 3, fileUrls: [] },  
 ];
 
 export const WorkspaceChatProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
